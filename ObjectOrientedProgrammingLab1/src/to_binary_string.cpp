eca8b111a438a9cfc45acf4f8979a0c0440e1333
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-09-21 10:05:24 +0300

    custom realization of to_binary_string; cmake now checks if GTest is installed; made unit_tests more readable

--- ObjectOrientedProgrammingLab1/src/to_binary_string.cpp
+++ ObjectOrientedProgrammingLab1/src/to_binary_string.cpp
@@ -2,6 +2,12 @@
 #include "to_binary_string.h"
 
 std::string to_binary_string(uint32_t n) {
-    std::bitset<32> binary(n);
-    return binary.to_string();
+    // std::bitset < 32> binary(n);
+    // std::string result = binary.to_string();
+    std::string result = "";
+    for (size_t i = 0; i < 32; ++i) {
+        result = std::to_string(n % 2) + result;
+        n /= 2;
+    }
+    return result;
 }
--- ObjectOrientedProgrammingLab1/tests/unit_tests.cpp
+++ ObjectOrientedProgrammingLab1/tests/unit_tests.cpp
@@ -1,25 +1,40 @@
-#include "../src/to_binary_string.cpp"
+#include "to_binary_string.h"
 #include <gtest/gtest.h>
 #include <string>
 
-TEST(ToBinaryStringTest, Test1) {
-    EXPECT_EQ(to_binary_string(1), "00000000000000000000000000000001");
+TEST(to_binary_string_test1, test1) {
+    int num = 1;
+    std::string expected = "00000000000000000000000000000001";
+    std::string result = to_binary_string(num);
+    EXPECT_EQ(result, expected);
 }
 
-TEST(ToBinaryStringTest, Test0) {
-    EXPECT_EQ(to_binary_string(0), "00000000000000000000000000000000");
+TEST(to_binary_string_test2, test0) {
+    int num = 0;
+    std::string expected = "00000000000000000000000000000000";
+    std::string result = to_binary_string(num);
+    EXPECT_EQ(result, expected);
 }
 
-TEST(ToBinaryStringTest, Test255) {
-    EXPECT_EQ(to_binary_string(255), "00000000000000000000000011111111");
+TEST(to_binary_string_test3, test255) {
+    int num = 255;
+    std::string expected = "00000000000000000000000011111111";
+    std::string result = to_binary_string(num);
+    EXPECT_EQ(result, expected);
 }
 
-TEST(ToBinaryStringTest, TestMaxUInt32) {
-    EXPECT_EQ(to_binary_string(4294967295), "11111111111111111111111111111111");
+TEST(to_binary_string_test4, test_max_uint32) {
+    int num = 4294967295;
+    std::string expected = "11111111111111111111111111111111";
+    std::string result = to_binary_string(num);
+    EXPECT_EQ(result, expected);
 }
 
-TEST(ToBinaryStringTest, TestRandom) {
-    EXPECT_EQ(to_binary_string(1024), "00000000000000000000010000000000");
+TEST(to_binary_string_test5, test_random) {
+    int num = 1024;
+    std::string expected = "00000000000000000000010000000000";
+    std::string result = to_binary_string(num);
+    EXPECT_EQ(result, expected);
 }
 
 int main(int argc, char **argv) {
