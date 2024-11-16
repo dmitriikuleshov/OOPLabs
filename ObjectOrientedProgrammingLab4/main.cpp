a02afc15d0a6ed10fbccb800bb96b065a6723118
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-11-16 10:18:22 +0300

    lab4: changed main programm

--- ObjectOrientedProgrammingLab4/main.cpp
+++ ObjectOrientedProgrammingLab4/main.cpp
@@ -1,37 +1,42 @@
-// Var 6
 #include "array.hpp"
 #include "flat_vector.hpp"
 #include "polygonal.hpp"
+#include <iostream>
+#include <memory>
 
 int main() {
-    Polygonal<double> p1{{0, 0}, {1, 1}, {0, 2}};
-    Polygonal<double> p2{{0, 0}, {4, 0}, {4, 3}, {0, 3}};
-    Polygonal<double> p3{{1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 3}};
-    Array<Polygonal<double>> arr;
+    auto p1 = std::make_shared<Polygonal<double>>(
+        Polygonal<double>{{0, 0}, {1, 1}, {0, 2}});
+    auto p2 = std::make_shared<Polygonal<double>>(
+        Polygonal<double>{{0, 0}, {4, 0}, {4, 3}, {0, 3}});
+    auto p3 = std::make_shared<Polygonal<double>>(
+        Polygonal<double>{{1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 3}});
+
+    auto arr = Array<std::shared_ptr<Figure<double>>>();
     arr.push_back(p1);
     arr.push_back(p2);
     arr.push_back(p3);
 
     for (size_t i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i] << std::endl;
+        std::cout << *arr[i] << std::endl;
     }
 
     std::cout << "Erasing second element" << std::endl;
     arr.erase(1);
 
     for (size_t i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i] << std::endl;
+        std::cout << *arr[i] << std::endl;
     }
 
     std::cout << "Inserting second element back" << std::endl;
     arr.insert(1, p2);
 
     for (size_t i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i] << std::endl;
+        std::cout << *arr[i] << std::endl;
     }
 
     for (size_t i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i].area() << std::endl;
+        std::cout << arr[i]->area() << std::endl;
     }
 
     Polygonal<int> p4{{1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 20}};
@@ -39,7 +44,8 @@ int main() {
     std::cout << "int {1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 20} polygonal area: "
               << p4.area() << std::endl;
     std::cout
-        << "float {1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 20} polygonal area: "
+        << "double {1, 1}, {3, 1}, {4, 3}, {2, 5}, {0, 20} polygonal area: "
         << p5.area() << std::endl;
+
     return 0;
 }
