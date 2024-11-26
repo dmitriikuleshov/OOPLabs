642dcf4ff9b47eb2a02c51f88c6b1e234ea43c49
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-11-26 17:47:57 +0300

    lab7 moved world configurator implementation to .cpp

--- ObjectOrientedProgrammingLab7/include/game/world_configurator.hpp
+++ ObjectOrientedProgrammingLab7/include/game/world_configurator.hpp
@@ -16,29 +16,18 @@
 
 class WorldConfigurator {
   public:
-    WorldConfigurator() {
-        define_npc_properties_config();
-        define_field_config();
-        define_attacker_visitors();
-        define_factories();
-        define_npc_generator();
-        define_field_size();
-        generate_npcs();
-    }
+    WorldConfigurator();
 
-    static ptr<WorldConfigurator> create() {
-        return std::make_shared<WorldConfigurator>();
-    }
+    static ptr<WorldConfigurator> create();
+    std::vector<ptr<NPC>> &get_npcs();
+    ptr<AttackerVisitor> get_attacker_visitor(NpcType type) const;
+    int get_max_x() const;
+    int get_max_y() const;
 
-    std::vector<ptr<NPC>> get_npcs() const { return npcs; }
+    std::vector<ptr<NPC>> npcs;
 
-    ptr<AttackerVisitor> get_attacker_visitor(NpcType type) const {
-        return attacker_visitors.at(type);
-    }
-    int get_max_x() const { return field_max_x; }
-    int get_max_y() const { return field_max_y; }
-
-    std::shared_mutex mtx;
+   private:
+    // std::shared_mutex mtx;
     // npc config
     std::string npc_properties_config_file_path_env = "NPC_PROPERTIES_CONFIG";
     std::string npc_properties_config_file_path;
@@ -59,79 +48,16 @@ class WorldConfigurator {
     std::unordered_map<NpcType, ptr<NpcFactory>> npc_factories;
     ptr<NpcGenerator> npc_generator;
     std::unordered_map<NpcType, ptr<AttackerVisitor>> attacker_visitors;
-    std::vector<ptr<NPC>> npcs;
 
-    // Methods
-    std::string get_config_file_path(const std::string &env) {
-        const char *path_raw = std::getenv(env.c_str());
-        if (!path_raw) {
-            throw std::runtime_error(
-                "Could not get config file path using environment variable");
-        }
-        return std::string(path_raw);
-    }
-
-    void define_npc_properties_config() {
-        npc_properties_config_file_path =
-            get_config_file_path(npc_properties_config_file_path_env);
-        npc_properties_config = NpcPropertiesConfigHandler::create_config(
-            npc_properties_config_file_path);
-        if (!npc_properties_config) {
-            throw std::runtime_error(
-                "Error while trying to get npc_properties_config");
-        }
-    }
-
-    void define_npc_generator() {
-        npc_generator = NpcGenerator::create(npc_factories);
-        if (!npc_generator) {
-            throw std::runtime_error("Error while trying to get npc_generator");
-        }
-    }
-
-    void define_field_config() {
-        field_config_file_path =
-            get_config_file_path(field_config_file_path_env);
-        field_config =
-            FieldConfigHandler::create_config(field_config_file_path);
-        if (!field_config) {
-            throw std::runtime_error("Error while trying to get field_config");
-        }
-    }
-
-    void define_attacker_visitors() {
-        attacker_visitors[KnightType] =
-            KnightAttackerVisitor::create(npc_properties_config);
-        attacker_visitors[DragonType] =
-            DragonAttackerVisitor::create(npc_properties_config);
-        attacker_visitors[FrogType] =
-            FrogAttackerVisitor::create(npc_properties_config);
-    }
-
-    void define_factories() {
-        npc_factories[KnightType] =
-            KnightFactory::create(npc_properties_config);
-        npc_factories[DragonType] =
-            DragonFactory::create(npc_properties_config);
-        npc_factories[FrogType] = FrogFactory::create(npc_properties_config);
-    }
-
-    void define_field_size() {
-        field_max_x = field_config->get_field_max_x();
-        field_max_y = field_config->get_field_max_y();
-    }
-
-    void generate_npcs() {
-        npcs_field_data = field_config->get_field();
-        for (const auto &data : npcs_field_data) {
-            auto npc =
-                npc_generator->create_npc(data.type, data.name, data.x, data.y);
-            if (!npc) {
-                throw std::runtime_error("Error creating npc with generator");
-            }
-            npcs.push_back(npc);
-        }
-    }
+    // world configurator setup
+    std::string get_config_file_path(const std::string &env);
+    void setup_npc_properties_config();
+    void setup_npc_generator();
+    void setup_field_config();
+    void setup_attacker_visitors();
+    void setup_factories();
+    void setup_field_size();
+    void generate_npcs();
 };
 
 #endif // WORLD_CONFIGURATOR
