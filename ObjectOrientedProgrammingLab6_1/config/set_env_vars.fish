90128d80b74da269bfa887021da0a62a26bd0daf
dmitriikuleshov <dmkuleshov10@gmail.com>
2024-11-28 21:08:13 +0300

    lab6_1: init

---
+++ ObjectOrientedProgrammingLab6_1/config/set_env_vars.fish
@@ -0,0 +1,4 @@
+# set_env_vars.fish
+
+set -x NPC_PROPERTIES_CONFIG /home/home/ObjectOrientedProgramming/ObjectOrientedProgrammingLab7/config/npc_properties.json
+set -x FIELD_CONFIG /home/home/ObjectOrientedProgramming/ObjectOrientedProgrammingLab7/config/field.json
